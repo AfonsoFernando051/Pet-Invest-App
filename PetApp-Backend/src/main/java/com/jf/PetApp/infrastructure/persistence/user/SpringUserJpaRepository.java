9c38a4f8bb16d49ad46be3c3fb532ed7dd54afce
AfonsoFernando051 <afonsofernando051@gmail.com>
2026-01-20 23:15:14 -0300

    Turning the interface public and adding method for save

--- PetApp-Backend/src/main/java/com/jf/PetApp/infrastructure/persistence/user/SpringUserJpaRepository.java
+++ PetApp-Backend/src/main/java/com/jf/PetApp/infrastructure/persistence/user/SpringUserJpaRepository.java
@@ -1,10 +1,15 @@
 package com.jf.PetApp.infrastructure.persistence.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jf.PetApp.domain.User;
+
 import java.util.Optional;
 
-interface SpringUserJpaRepository
+public interface SpringUserJpaRepository
         extends JpaRepository<UserJpaEntity, Integer> {
 
     Optional<UserJpaEntity> findByEmail(String email);
+
+	void save(User user);
 }
