3667a605d54b1fe4b9a7b0bdf78ff8cef7d2c98f
AfonsoFernando051 <afonsofernando051@gmail.com>
2026-01-20 23:15:38 -0300

    Adding the first test class

---
+++ PetApp-Backend/src/test/java/com/jf/PetApp/PersistenceTest.java
@@ -0,0 +1,41 @@
+package com.jf.PetApp;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+import com.jf.PetApp.domain.User;
+import com.jf.PetApp.infrastructure.persistence.user.SpringUserJpaRepository;
+import com.jf.PetApp.infrastructure.persistence.user.UserJpaEntity;
+
+@DataJpaTest
+class PersistenceTest {
+	
+		@Autowired
+	    private SpringUserJpaRepository repository;
+
+	    @Test
+	    void should_save_and_find_user_by_email() {
+	        // given
+	    	User user = new User();
+		    user.setUsername("ana");
+		    user.setEmail("ana@test.com");
+		    user.setPassword("123");
+		        
+	        UserJpaEntity userRepoEntity = UserJpaEntity.fromDomain(user);
+
+	        // when
+	        repository.save(userRepoEntity);
+
+	        // then
+	        var found = repository.findByEmail("ana@test.com");
+
+	        assertThat(found).isPresent();
+//	        assertThat(found.get().getUsername()).isEqualTo("ana");
+	    }
+	    
+	
+
+}
